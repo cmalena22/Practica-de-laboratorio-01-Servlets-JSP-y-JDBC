f27015e9e85c0223666b002f85cbedb32118cbff
Cinthia iza <cizac1@est.ups.edu.ec>
2020-05-06 01:20:24 -0500

    Configuracion

---
+++ PracticaJEE/src/ec/ups/edu/controlador/IniciarSesionServlet.java
@@ -0,0 +1,67 @@
+package ec.ups.edu.controlador;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class IniciarSesionServlet
+ */
+@WebServlet("/IniciarSesionServlet")
+public class IniciarSesionServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public IniciarSesionServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		HttpSession session=request.getSession(true);
+		String nombre="";
+		String contra="";
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print("<html><head><title>Inicio Sesion</title></head><body>");
+		out.print("<p> Parametros , añadir  a la URL : ?nombre=admin&contra=admin</p>");
+		
+		nombre=request.getParameter("nombre");
+		out.println("<h1> Nombre:"+ nombre+ "</h1>");
+		
+		contra=request.getParameter("contra");
+		out.println("<h1> Contraseña:"+ contra+ "</h1>");		
+		if (nombre!=null && contra!=null) {	
+			if ("admin".equals(nombre) &&"admin".equals(contra) ) {
+				out.println("<h1> Bienvenido:"+ nombre+ "</h1>");
+				//para pasar de un servlet a otro servlet
+				response.sendRedirect(request.getContextPath()+"/Calculadora");
+			}else{
+				out.println("<h1>Chao inicia sesion de nuevo</h1>");
+			
+			}
+			
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
