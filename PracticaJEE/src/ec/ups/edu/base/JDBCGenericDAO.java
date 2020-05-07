b17a4ffda74297c29d5e00dd84b40c031f65c3f1
Cinthia iza <cizac1@est.ups.edu.ec>
2020-05-07 12:25:11 -0500

    Creando la interfaz publica

--- PracticaJEE/src/ec/ups/edu/base/JDBCGenericDAO.java
+++
@@ -1,8 +0,0 @@
-package ec.ups.edu.base;
-
-import ec.ups.edu.controlador.GenericDAO;
-
-public abstract class JDBCGenericDAO <T, ID> implements GenericDAO<T, ID> {
-	protected ContextJDBC conexionUno = ContextJDBC.getJDBC1();
-	protected ContextJDBC conexionDos = ContextJDBC.getJDBC2();
-}
--- PracticaJEE/src/ec/ups/edu/controlador/PersonaDAO.java
+++
@@ -1,7 +0,0 @@
-package ec.ups.edu.controlador;
-
-import ec.edu.ups.modelo.Usuario;
-
-public interface PersonaDAO extends GenericDAO<Usuario, Integer>{
-
-}
